ef192cb384e01fb60d7bf37fe9540e7ba6534229
Manish Kushare <manish.altcampus@gmail.com>
2021-11-08 09:50:27 +0530

    done

---
+++ block-BRaaaq/code/gizmo/src/Components/App.js
@@ -0,0 +1,21 @@
+
+import Header from './Header';
+import Hero from './Hero';
+import Articles from "./Articles";
+import Footer from './Footer';
+import {Article } from './Articles';
+
+function App(){
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Articles />
+      <Footer />
+    </>
+  )
+};
+
+
+
+export default App;
